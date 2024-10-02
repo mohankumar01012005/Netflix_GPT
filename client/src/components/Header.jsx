81b72b2ed047d0d0bbc322cce45b2be056ff0329
mohankumar01012005 <kumar.mohan@kalvium.community>
2024-10-02 12:37:16 +0530

    completed authentication and done with login and logout functionalities

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
+import { signOut } from "firebase/auth";
+
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Header() {
-  return (
-   <div className='absolute px-8 py-2 bg-gradient-to-b from-black z-10'>
-     <img className='w-44' src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'></img>
-   </div>
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
+  
+  const handleSignout = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        navigate("/");
+      })
+      .catch((error) => {
+        // An error happened.
+        navigate("/error");
+      });
+  };
 
-  )
+  return (
+    <>
+      <div className="absolute w-screen  px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
+        <img
+          className="w-44"
+          src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        ></img>
+     {user &&(  <div className="flex  align-middle">
+        
+        <img
+          alt="userIcon "
+          src={user?.photoURL}
+          className="w-8 h-8 mt-4"
+        ></img>
+   
+        <button onClick={handleSignout} className="font-bold text-white p-2 ">
+          Sign Out{" "}
+        </button>  
+      </div>)}
+      </div>
+    </>
+  );
 }
 
-export default Header
+export default Header;
--- client/src/utils/validate.js
+++ client/src/utils/validate.js
@@ -1,12 +1,21 @@
-export const checkvalidData=(name,email,password)=>{
+export const checkvalidDataSignUp = (name, email, password) => {
+  const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email);
+  const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
+  const isNameValid = /^[A-Za-z][A-Za-z0-9_]{7,29}$/.test(name);
 
-const isEmailValid=/^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email)
-const isPasswordValid=/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password)
-const isnameValid= /^[A-Za-z][A-Za-z0-9_]{7,29}$/.test(name)
+  if (!isNameValid) return "User Name is not valid";
+  if (!isEmailValid) return "Email address is not valid";
+  if (!isPasswordValid) return "Password is not valid";
 
-if(!isnameValid)return  "User Name is not Valid"
-if(!isEmailValid)return "Email address is not valid ";
-if(!isPasswordValid)return "Password is not valid ";
+  return null;
+};
 
-return null;
-}
+export const checkvalidDataSignIn = (email, password) => {
+  const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email);
+  const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
+
+  if (!isEmailValid) return "Email address is not valid";
+  if (!isPasswordValid) return "Password is not valid";
+
+  return null;
+};
